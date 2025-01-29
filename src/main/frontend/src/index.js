0008533fd81d945a2c9332e8029f93b487cf0d8f
Juste Bouithy <jrb518@ensign.edu>
2025-01-29 16:13:11 -0700

    Reconfiguration of Amplify

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Page2 from './Page2';
-import Navbar from "./Navbar";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import App from './Components/App';
+import Page2 from "./Components/Page2";
+import Navbar from "./Components/Navbar";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
 
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +16,8 @@ root.render(
     <BrowserRouter>
         <Navbar/>
         <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route path="Page2" element={<Page2/>}/>
+            <Route path="/" element={<App />} />
+            <Route path="/page2" element={<Page2 />} />
         </Routes>
     </BrowserRouter>
   </React.StrictMode>
